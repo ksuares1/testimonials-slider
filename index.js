abdf79db5484a29bd9ee2ac241c8a9054dcc3ffb
ksuares1 <kimberlyjsuares@gmail.com>
2023-03-04 09:47:16 -0500

    updated slide 2

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-$(document).ready(function () {
-  $(".n").click(function () {
-    $(".testimonial-slider").toggle(200);
-    $(".content2").toggle(600);
-    $(".a1").toggleClass("disabled");
-    $(".a2").toggleClass("disabled");
-  });
-  $(".p").click(function () {
-    $(".testimonial-slider").toggle(200);
-    $(".content2").toggle(600);
-    $(".a1").toggleClass("disabled");
-    $(".a2").toggleClass("disabled");
-  });
-});
+
